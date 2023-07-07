5c28ac3535aed3b309122315497a7e3d7a9b25c0
Hiroki <h.m.jojo.8258@gmail.com>
2023-07-07 00:44:04 -0700

    done axios setting

--- client/src/api/axiosClient.js
+++ client/src/api/axiosClient.js
@@ -1,7 +1,26 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/v1"
+const getToken = () => localStorage.getItem("token");
 
 const axiosClient = axios.create({
     baseURL: BASE_URL,
 });
+
+axiosClient.interceptors.request.use(async(config) => {
+    return{
+        config,
+        headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${getToken()}`, //send JWT to server after adding it at request header
+        },
+    }
+});
+
+axiosClient.interceptors.responce.use((response) => {
+    return response;
+}, (err) => {
+    throw err.responce;
+});
+
+export default axiosClient;
