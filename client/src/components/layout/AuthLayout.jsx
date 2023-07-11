7ac8de399f60b14712347ed31e1537049db97ed0
Hiroki <h.m.jojo.8258@gmail.com>
2023-07-10 22:12:05 -0700

    made verify JWT

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,9 +1,23 @@
 import { Box, Container } from '@mui/material';
 import React from 'react'
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 import notionLogo from "../../assets/images/notion-logo.png";
+import { useEffect } from 'react';
+import authUtils from '../../utils/authUtils';
+
 
 const AuthLayout = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const isAuth = await authUtils.isAuthenticated();
+            if(isAuth){
+                navigate("/");
+            }
+        };
+        checkAuth();
+    }, [navigate]);
   return (
     <div>
         <Container component="main" maxWidth="xs">
